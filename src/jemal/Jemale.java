567df69e31ddad297afc9d0aa379efb1a8c24426
maha <mahameihemid@gmail.com>
2018-11-16 20:27:50 +0100

    gh

---
+++ src/jemal/Jemale.java
@@ -0,0 +1,11 @@
+package jemal;
+
+public class Jemale {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("c");
+
+	}
+
+}
---
+++ src/jemal/hh.java
@@ -0,0 +1,10 @@
+package jemal;
+
+public class hh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
